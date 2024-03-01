29f110b84ee08b4358894b41f707c7597740b4e8
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-01 13:16:15 -0300

    Redis added and configured

---
+++ server/src/config/redis.js
@@ -0,0 +1,15 @@
+import { createClient } from "redis";
+
+export const client = await createClient({
+  url: process.env.REDIS_URL,
+})
+  .on("error", (err) =>
+    console.log(
+      "Redis Client Error.",
+      err
+    )
+  )
+  .on("connect", () => {
+    console.log("Redis connected.");
+  })
+  .connect();
