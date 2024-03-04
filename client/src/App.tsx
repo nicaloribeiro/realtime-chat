c483d519f6b52d48b6fc22a93279e647cc8effbf
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-04 18:16:27 -0300

    Using routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,56 +1,13 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { socket } from "./config/socket";
+import { useSelector } from "react-redux";
+import { RootState } from "./modules/common/store/reducers";
+import { SignedInRoutes, SignedOutRoutes } from "./modules/common/routes";
+import { UserInitialState } from "./modules/common/store/reducers/user/user-types";
 
-function App() {
-  const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
+const App = () => {
+  const user = useSelector<RootState, UserInitialState>(({ user }) => user);
 
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-    };
-  }, []);
-
-  const connect = () => {
-    socket.connect();
-  };
-
-  const disconnect = () => {
-    socket.disconnect();
-  };
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => (isConnected ? disconnect() : connect())}>
-          {isConnected ? "Connected" : "Disconnected"}
-        </button>
-      </div>
-    </>
-  );
-}
+  return user.user.accessToken ? <SignedInRoutes /> : <SignedOutRoutes />;
+};
 
 export default App;
