bb499da67b27ae84a2bb466c575baf9a1bc1002f
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-06 00:03:40 -0300

    Tailwind colors config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-bg": "#121212",
+        "dark-secondary": "#1E1E1E",
+        "text-primary": "#E0E0E0",
+        "text-secondary": "#B3B3B3",
+        highlight: "#BB86FC",
+        warning: "#CF6679",
+        success: "#4CAF50",
+        info: "#64B5F6",
+        "info-dark": "#42A5F5",
+        "border-color": "#292929",
+        "success-dark": "#388E3C",
+        "warning-dark": "#A64453",
+      },
+      backgroundImage: {
+        'logo': "url('/src/assets/logo-3.jpeg')",
+      },
+    },
   },
   plugins: [],
 };
