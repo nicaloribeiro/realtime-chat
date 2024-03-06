0912fc36143d8ba9efed6a8dfd60d2c243962e78
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-06 00:05:33 -0300

    Importing toastify styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./modules/common/store/index.ts";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
