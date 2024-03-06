b4c5d440b8478621cebcc0bf7a20afebec7794eb
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-06 00:10:18 -0300

    Signed out base layout created

--- client/src/modules/layouts/SignedOutLayout.tsx
+++ client/src/modules/layouts/SignedOutLayout.tsx
@@ -2,11 +2,8 @@ import { Outlet } from "react-router-dom";
 
 const SignedOutLayout = () => {
   return (
-    <div className="flex flex-col w-screen h-screen lg:flex-row lg:flex bg-red-200">
-      <div>a</div>
-      <div className="w-full h-full px-8 pt-8 overflow-y-auto bg-fuchsia-600">
-        <Outlet />
-      </div>
+    <div className="flex w-screen h-screen bg-dark-bg items-center justify-center">
+      <Outlet />
     </div>
   );
 };
