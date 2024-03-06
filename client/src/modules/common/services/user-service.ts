6ac3d11bc439621846cce1b1a1beb8e6bdfec09c
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-06 00:08:04 -0300

    Register service created

--- client/src/modules/common/services/user-service.ts
+++ client/src/modules/common/services/user-service.ts
@@ -1,5 +1,9 @@
 import api from "../../../config/api";
-import { LoginOutput } from "../types/login-types";
+import {
+  LoginOutput,
+  RegisterInput,
+  RegisterOutput,
+} from "../types/login-types";
 
 const login = async (email: string, password: string): Promise<LoginOutput> => {
   const { data } = await api.post<LoginOutput>("/api/account/login", {
@@ -9,5 +13,13 @@ const login = async (email: string, password: string): Promise<LoginOutput> => {
   return data;
 };
 
-const UserService = { login };
+const register = async (payload: RegisterInput): Promise<RegisterOutput> => {
+  const { data } = await api.post<RegisterOutput>(
+    "/api/account/register",
+    payload
+  );
+  return data;
+};
+
+const UserService = { login, register };
 export default UserService;
