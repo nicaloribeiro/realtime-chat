e4251dd0ca17e641befe5a7e0edb16f6f3e4d26f
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-06 00:06:49 -0300

    Logout component created

--- client/src/modules/common/components/Logout.tsx
+++ client/src/modules/common/components/Logout.tsx
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
-import { logout } from "../../store/reducers/user/user-reducer";
-import { AppDispatch } from "../../store";
+import { logout } from "../store/reducers/user/user-reducer";
+import { AppDispatch } from "../store";
 
 const Logout = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -11,7 +11,7 @@ const Logout = () => {
 
   return (
     <button className="text-green-200" onClick={() => handleLogout()}>
-      Logoout
+      Logout
     </button>
   );
 };
