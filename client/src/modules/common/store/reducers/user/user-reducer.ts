293982d188bd5a61c5f3e32a7566a7b43b7b36ae
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-06 00:08:51 -0300

    New thunks created

--- client/src/modules/common/store/reducers/user/user-reducer.ts
+++ client/src/modules/common/store/reducers/user/user-reducer.ts
@@ -1,8 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { User, UserInitialState } from "./user-types";
 import { jwtDecode } from "jwt-decode";
-import { LoginInput } from "../../../types/login-types";
+import {
+  LoginInput,
+  RegisterInput,
+  RegisterOutput,
+} from "../../../types/login-types";
 import UserService from "../../../services/user-service";
+import { toast } from "react-toastify";
 
 const initialState: UserInitialState = {
   user: {
@@ -49,6 +54,13 @@ export const logout = createAsyncThunk("user/logout", async () => {
   localStorage.removeItem("access_token");
 });
 
+export const register = createAsyncThunk(
+  "user/register",
+  async (payload: RegisterInput): Promise<RegisterOutput> => {
+    return await UserService.register(payload);
+  }
+);
+
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -66,8 +78,10 @@ const userSlice = createSlice({
       .addCase(login.pending, (state) => {
         state.loading = true;
       })
-      .addCase(login.rejected, (state) => {
+      .addCase(login.rejected, (state, err) => {
+        console.log(err);
         state.loading = false;
+        toast.warning("User not found");
       })
       .addCase(login.fulfilled, (state, action) => {
         const user = action.payload;
@@ -76,6 +90,16 @@ const userSlice = createSlice({
       })
       .addCase(logout.fulfilled, () => {
         return initialState;
+      })
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(register.rejected, (state) => {
+        state.loading = false;
+      })
+      .addCase(register.fulfilled, (state) => {
+        state.loading = false;
+        toast.success("Registration completed!");
       });
   },
 });
