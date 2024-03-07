e94a9ef2323fec9ef605fa769a729edfcfbf3496
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-07 01:27:36 -0300

    Adjustments on signed in routes

--- client/src/modules/common/routes/signedin-routes.tsx
+++ client/src/modules/common/routes/signedin-routes.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Logout from "../components/Logout";
+import SignedInLayout from "../../layouts/SignedInLayout";
 
 const signedInRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Logout />,
+    element: <SignedInLayout />,
     children: [
+      {
+        index: true,
+        element: <Logout />,
+      },
       {
         path: "/*",
         element: <h1>Not Found.</h1>,
