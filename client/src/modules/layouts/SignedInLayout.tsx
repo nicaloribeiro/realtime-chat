38ef628ab9ab95ef2182092e16baff2aa29ff180
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-11 23:45:37 -0300

    NavBar base config

--- client/src/modules/layouts/SignedInLayout.tsx
+++ client/src/modules/layouts/SignedInLayout.tsx
@@ -4,8 +4,14 @@ import { AppDispatch } from "../common/store";
 import { useDispatch } from "react-redux";
 import { socketConnect } from "../common/store/reducers/user/user-reducer";
 import NavBar from "../navbar/NavBar";
+import UserProfile from "../navbar/components/UserLogo";
+import { useSelector } from "react-redux";
+import { RootState } from "../common/store/reducers";
+import { UserInitialState } from "../common/store/reducers/user/user-types";
+import SearchUser from "../navbar/components/SearchUser";
 
 const SignedInLayout = () => {
+  const user = useSelector<RootState, UserInitialState>(({ user }) => user);
   const dispatch: AppDispatch = useDispatch();
 
   useEffect(() => {
@@ -15,7 +21,17 @@ const SignedInLayout = () => {
   return (
     <div className="flex w-screen h-screen bg-dark-bg items-center justify-center relative">
       <div className="absolute top-0 w-full">
-        <NavBar>Nav</NavBar>
+        <NavBar>
+          <div className="col-span-1 col-start-1 h-12 flex items-center justify-start">
+            <img src="/src/assets/logo-3.jpeg" className="h-full" />
+          </div>
+          <div className="col-span-6 col-start-4 flex items-center justify-center">
+            <SearchUser />
+          </div>
+          <div className="col-span-1 flex items-center justify-end col-end-13  px-4">
+            <UserProfile user={user.user.name || "?"} />
+          </div>
+        </NavBar>
       </div>
       <Outlet />
     </div>
