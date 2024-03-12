b5e447e82c7d16ea9be739c5965d442c37ee6c2e
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-11 23:45:52 -0300

    Default nav bar created

--- client/src/modules/navbar/NavBar.tsx
+++ client/src/modules/navbar/NavBar.tsx
@@ -1,12 +1,9 @@
-import UserProfile from "./components/UserLogo";
-
 type NavBarProps = {
   children?: React.ReactNode;
 };
 const NavBar = ({ children }: NavBarProps) => {
   return (
-    <div className="flex items-center p-2 w-full h-12 bg-dark-secondary">
-      <UserProfile />
+    <div className="grid grid-cols-12 w-full h-12 bg-dark-secondary">
       {children}
     </div>
   );
