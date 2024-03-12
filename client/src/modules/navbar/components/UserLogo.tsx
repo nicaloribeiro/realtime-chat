c5618adb6be0737be2714a57a392e41309823df2
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-11 23:47:25 -0300

    Default user logo created

--- client/src/modules/navbar/components/UserLogo.tsx
+++ client/src/modules/navbar/components/UserLogo.tsx
@@ -1,10 +1,42 @@
-import { generateRandomColors } from "../../common/utils/colors";
+import { Dropdown, MenuProps } from "antd";
+import Logout from "../../common/components/Logout";
 
-const UserProfile = () => {
+type UserProfileProps = {
+  user: string;
+  children?: React.ReactNode;
+};
+const UserProfile = ({ user }: UserProfileProps) => {
+  const items: MenuProps["items"] = [
+    {
+      label: user,
+      key: "username",
+    },
+    {
+      label: <a>Profile</a>,
+      key: "0",
+    },
+    {
+      type: "divider",
+    },
+    {
+      danger: true,
+      label: <Logout />,
+      key: "3",
+    },
+  ];
   return (
-    <div className={`flex items-center justify-center h-10 w-10 rounded-full bg-[#${generateRandomColors()}] border border-border-color`}>
-      L
-    </div>
+    <Dropdown
+      menu={{ items }}
+      trigger={["click"]}
+      className="hover:cursor-pointer"
+      placement="bottom"
+    >
+      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-dark-bg border border-border-color">
+        <a onClick={(e) => e.preventDefault()}>
+          <span className="select-none">{user[0]}</span>
+        </a>
+      </div>
+    </Dropdown>
   );
 };
 
