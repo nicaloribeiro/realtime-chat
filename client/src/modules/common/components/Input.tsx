7dffe391c9b423d10231cd8ddcfac48a349ff1ce
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-11 23:43:42 -0300

    Adjustments on input component

--- client/src/modules/common/components/Input.tsx
+++ client/src/modules/common/components/Input.tsx
@@ -2,15 +2,33 @@ import { ReactNode } from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   leftIcon?: ReactNode;
+  marginY?: number;
+  marginX?: number;
+  padding?: number;
+  innerPadding?: number;
+  placeholder?: string;
+  rounded?: string
 }
 
-const Input = ({ leftIcon, ...props }: InputProps) => {
+const Input = ({
+  leftIcon,
+  marginY = 2,
+  marginX = 0,
+  padding = 3,
+  innerPadding = 0,
+  placeholder = "",
+  rounded = "md",
+  ...props
+}: InputProps) => {
   return (
-    <div className="w-full rounded-md p-3 my-2 focus:border-highlight border border-none flex items-center justify-start bg-[#3A3D3F]">
+    <div
+      className={`w-full rounded-${rounded} p-${padding} my-${marginY} mx-${marginX} focus:border-highlight border border-none flex items-center justify-start bg-[#3A3D3F]`}
+    >
       {leftIcon && <div className="pr-2">{leftIcon}</div>}
       <input
         {...props}
-        className="w-full placeholder:italic placeholder:text-slate-400 focus:outline-none bg-transparent"
+        className={`w-full placeholder:italic placeholder:text-slate-400 focus:outline-none bg-transparent p-${innerPadding}`}
+        placeholder={placeholder}
       />
     </div>
   );
