fc553ef65e095701d55da43f16c09fa3202334b2
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-11 23:48:00 -0300

    User model updated

--- server/src/user/user-model.js
+++ server/src/user/user-model.js
@@ -4,14 +4,15 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   friendList: { type: [String], default: [] },
-  isOnline: { type: Boolean, default: false },
   name: { type: String, required: true },
   password: { type: String, required: true },
-  socketId: { type: String, default: "" },
   username: { type: String, required: true },
+  blockList: { type: [String], default: [] },
+  friendRequest: { type: [String], default: [] },
 });
 
 UserSchema.index({ email: 1 });
+UserSchema.index({ blockList: 1 });
 
 const User = mongoose.model("User", UserSchema);
 
