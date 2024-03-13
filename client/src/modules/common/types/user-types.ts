dfd312f7153b185aa2404ef5df40cd750f40c0ac
Nicalo Ribeiro <nicalo63@gmail.com>
2024-03-13 01:34:56 -0300

    User types adjustments

--- client/src/modules/common/types/user-types.ts
+++ client/src/modules/common/types/user-types.ts
@@ -1,11 +1,12 @@
 export type UsersFound = {
   email: string;
   name: string;
+  username: string;
   friendRequestSent: boolean;
   isFriend: boolean;
 };
 
 export type UserSocketConnection = {
-    email: string
-    name: string
-}
+  email: string;
+  name: string;
+};
